fa6c9cd363f6c732144fc50a2fae261b401b0c3b
sggb <shangggb@joyowo.com>
2017-05-03 15:16:55 +0800

    /跳转到/hystrix

--- src/main/java/com/fishmeat/Application.java
+++ src/main/java/com/fishmeat/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author sggb
@@ -12,9 +14,15 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrixDashboard
+@Controller
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "forward:/hystrix";
     }
 }
